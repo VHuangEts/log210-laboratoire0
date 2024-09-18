a1d9407a8eae51ca7c3810fce5722c251a33256b
VHuangEts <vincen.huang.1@ens.etsmtl.ca>
2024-09-18 14:39:03 -0400

    3. Écrire des tests pour la fonctionnalité

--- test/lab0/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/lab0/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -1,9 +1,35 @@
 // Vous devez insérer les nouveaux tests ici
 import { assert } from 'console';
+import { application, response } from 'express';
 import 'jest-extended';
+import supertest from 'supertest'
+import app from '../../../src/app';
+
+const request = supertest(app);
 
 describe('redemarrerJeu.test.ts', () => {
   it("should implement test", async () => {
     throw new Error("Ce test n'a pas été défini")
   });
+});
+
+describe('GET /api/v1/jeu/redemarrerJeu', () => {
+  let joueur1 : any, joueur2 : any;
+
+  beforeAll(async () => {
+    joueur1 = await request.post('/api/v1/jeu/creerJoueur').send({nom : 'Joueur1'});
+    joueur2 = await request.post('/api/v1/jeu/creerJoueur').send({nom : 'Joueur2'});
+  })
+
+  it("devrait redémarrer le jeu avec succès et retourner un status 200", async () => {
+    const response = await request.get('/api/v1/jeu/redemarrerJeu');
+    expect(response.status).toBe(200);
+    expect(response.type).toBe("application/json");
+  });
+
+  it("devrait ne plus avoir de joueurs après redémarrage", async () => {
+    const joueursResponse = await request.get('/api/v1/jeu/joueurs');
+    expect(joueursResponse.body.length).toBe(0);
+  });
+
 });
